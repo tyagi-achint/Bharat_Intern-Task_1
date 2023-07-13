e18c4a858cc836fab2389482f138cc1a3aa75da4
tyagi-achint <achinttyagi001@gmail.com>
2023-07-13 19:00:04 +0530

    working

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 let form = document.querySelector("form");
-let icon = document.querySelector("#blogs .material-symbols-outlined");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -24,6 +23,7 @@ form.addEventListener("submit", (e) => {
   blogImage.src = image;
   blogPublisher.textContent = "By: " + publisher;
   deleteButton.textContent = "Delete";
+  deleteButton.addEventListener("click", deleteBlog);
 
   blogContainer.appendChild(blogTitle);
   blogContainer.appendChild(blogImage);
@@ -32,10 +32,13 @@ form.addEventListener("submit", (e) => {
   blogContainer.appendChild(deleteButton);
 
   let blogs = document.querySelector("#blogs");
+
+  let icon = document.querySelector("span.material-symbols-outlined");
   let activeCard = null;
 
   blogContainer.addEventListener("click", view);
-  deleteButton.addEventListener("click", deleteBlog);
+
+  icon.addEventListener("click", noview);
 
   function view() {
     if (activeCard !== null) {
@@ -48,107 +51,21 @@ form.addEventListener("submit", (e) => {
     activeCard = this;
   }
 
+  function noview() {
+    if (activeCard !== null) {
+      activeCard.classList.remove("expand");
+      blogContent.classList.toggle("notexpand");
+      activeCard = null;
+    }
+    icon.classList.remove("cut-icon");
+  }
+
   function deleteBlog() {
     blogs.removeChild(blogContainer);
     icon.classList.remove("cut-icon");
-    if (activeCard === blogContainer) {
-      activeCard = null;
-    }
   }
 
   blogs.appendChild(blogContainer);
 
   form.reset();
-
-  saveData(title, publisher, content, image);
-});
-
-icon.addEventListener("click", noview);
-
-function noview() {
-  let activeCard = document.querySelector(".expand");
-  if (activeCard !== null) {
-    activeCard.classList.remove("expand");
-    activeCard.querySelector("p").classList.toggle("notexpand");
-  }
-  icon.classList.remove("cut-icon");
-}
-
-function saveData(title, publisher, content, image) {
-  let blogsData = localStorage.getItem("blogsData");
-
-  if (blogsData) {
-    blogsData = JSON.parse(blogsData);
-  } else {
-    blogsData = [];
-  }
-
-  let blogData = {
-    title: title,
-    publisher: publisher,
-    content: content,
-    image: image,
-  };
-
-  blogsData.push(blogData);
-  localStorage.setItem("blogsData", JSON.stringify(blogsData));
-}
-
-window.addEventListener("load", () => {
-  let blogsData = localStorage.getItem("blogsData");
-
-  if (blogsData) {
-    blogsData = JSON.parse(blogsData);
-
-    blogsData.forEach((blogData) => {
-      let { title, publisher, content, image } = blogData;
-
-      let blogContainer = document.createElement("div");
-      let blogTitle = document.createElement("h2");
-      let blogPublisher = document.createElement("h4");
-      let blogContent = document.createElement("p");
-      let blogImage = document.createElement("img");
-      let deleteButton = document.createElement("button");
-      blogContent.classList.add("notexpand");
-
-      blogTitle.textContent = title;
-      blogContent.innerHTML = content;
-      blogImage.src = image;
-      blogPublisher.textContent = "By: " + publisher;
-      deleteButton.textContent = "Delete";
-
-      blogContainer.appendChild(blogTitle);
-      blogContainer.appendChild(blogImage);
-      blogContainer.appendChild(blogContent);
-      blogContainer.appendChild(blogPublisher);
-      blogContainer.appendChild(deleteButton);
-
-      let blogs = document.querySelector("#blogs");
-      let activeCard = null;
-
-      blogContainer.addEventListener("click", view);
-      deleteButton.addEventListener("click", deleteBlog);
-
-      function view() {
-        if (activeCard !== null) {
-          activeCard.classList.remove("expand");
-          blogContent.classList.toggle("notexpand");
-        }
-        this.classList.add("expand");
-        blogContent.classList.toggle("notexpand");
-        icon.classList.add("cut-icon");
-        activeCard = this;
-      }
-
-      function deleteBlog() {
-        blogs.removeChild(blogContainer);
-        icon.classList.remove("cut-icon");
-        if (activeCard === blogContainer) {
-          activeCard = null;
-        }
-      }
-
-      blogs.appendChild(blogContainer);
-    });
-  }
 });
